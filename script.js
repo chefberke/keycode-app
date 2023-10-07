1f4d833824cce25675418475bfa370f9d99a932b
Berke Kanber <berkekanber@gmail.com>
2023-10-07 17:14:41 +0300

    all files added

---
+++ script.js
@@ -0,0 +1,30 @@
+function showKeyCodes(e) {
+    const insert = document.getElementById('insert')
+    insert.innerHTML = '';
+
+    const keyCodes = {
+        'e.key': e.key === ' ' ? 'Space' : e.key,
+        'e.keyCode': e.keyCode,
+        'e.code': e.code
+    }
+
+    for(let key in keyCodes) {
+        const div = document.createElement('div')
+        div.className = 'box'
+        const small = document.createElement('small')
+
+        const keyText = document.createTextNode(key)
+        const valueText = document.createTextNode(keyCodes[key])
+
+        small.appendChild(valueText)
+        div.appendChild(keyText)
+        div.appendChild(small)
+
+        insert.appendChild(div)
+    }
+
+};
+
+// eventListener
+window.addEventListener('keydown', showKeyCodes);
+
